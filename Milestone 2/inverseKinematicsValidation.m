7ed09cf69787ff724a22579f4b1e78233f38ab77
Bishoy Atef Sadek Zaky <bishoy.zaky@student.guc.edu.eg>
2021-12-05 23:19:14 +0200

    Update inverseKinematicsValidation.m

--- Milestone 2/inverseKinematicsValidation.m
+++ Milestone 2/inverseKinematicsValidation.m
@@ -4,4 +4,6 @@ initialGuessAngles = [0.1 0.1 0.1 0.1];
 desiredPosition = [-45 -30 -500];
 outputAngles = inverse_kinematics_func(initialGuessAngles,desiredPosition);
 %Validating the output angles using the forward kinematics function
-outputPosition = subs(forward_kinematics_func,[q1 q2 q3 q4],outputAngles)
+[x,y,z] = forward_kinematics_func();
+F = [x;y;z];
+outputPosition = subs(F, [q1 q2 q3 q4], outputAngles)
