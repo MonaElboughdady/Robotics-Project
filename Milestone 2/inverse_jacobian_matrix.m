5c084fe46deda2fbdac2366db68415abfc9d9ed5
Abdo0saad <65568425+Abdo0saad@users.noreply.github.com>
2021-12-05 09:57:22 +0200

    Update inverse_jacobian_matrix.m

--- Milestone 2/inverse_jacobian_matrix.m
+++ Milestone 2/inverse_jacobian_matrix.m
@@ -1,11 +1,14 @@
 function J_inv = inverse_jacobian_matrix(q)
-[~,n] = size(q);
-J_inv = sym('A%d%d',[3 n]);
+syms q1 q2 q3 q4
+Symbols = [q1 q2 q3 q4];
+J_inv = sym('A%d%d',[3 4]);
 [X,Y,Z] = forward_kinematics_func();
- for j = 1:n
-   J_inv(1,j) = diff(X,q(j));
-   J_inv(2,j) = diff(Y,q(j));
-   J_inv(3,j) = diff(Z,q(j));
+ for j = 1:4
+   J_inv(1,j) = diff(X,Symbols(j));
+   J_inv(2,j) = diff(Y,Symbols(j));
+   J_inv(3,j) = diff(Z,Symbols(j));
  end
+J_inv = subs(J_inv ,[q1 q2 q3 q4],q);
+J_inv = simplify( J_inv );
 J_inv = pinv(J_inv);
 end
