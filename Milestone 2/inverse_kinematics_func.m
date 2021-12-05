bbc30cb5ceb0c87224e7039f30fba57af9621944
Abdo0saad <65568425+Abdo0saad@users.noreply.github.com>
2021-12-05 08:38:35 +0200

    Update inverse_kinematics_func.m

--- Milestone 2/inverse_kinematics_func.m
+++ Milestone 2/inverse_kinematics_func.m
@@ -1,13 +1,14 @@
 function q = inverse_kinematics_func(q0, X)
-F = forward_kinematics_func();
-F = F';
+syms q1 q2 q3 q4
+[x,y,z] = forward_kinematics_func();
+F = [x;y;z];
 F = subs(F ,[q1 q2 q3 q4],q0) - X;
 J_inv = inverse_jacobian_matrix(q0);
 q = q0 - J_inv * F;
 while q ~= q0
     q0 = q;
-    F = forward_kinematics_func();
-    F = F';
+    [x,y,z] = forward_kinematics_func();
+    F = [x;y;z];
     F = subs(F ,[q1 q2 q3 q4],q0) - X;
     J_inv = inverse_jacobian_matrix(q0);
     q = q0 - J_inv * F;
