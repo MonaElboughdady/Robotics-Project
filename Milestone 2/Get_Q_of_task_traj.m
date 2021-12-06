a7842e1db7097f0743410d8cf69f9de79436724f
Abdo0saad <65568425+Abdo0saad@users.noreply.github.com>
2021-12-06 03:52:51 +0200

    Update Get_Q_of_task_traj.m

--- Milestone 2/Get_Q_of_task_traj.m
+++ Milestone 2/Get_Q_of_task_traj.m
@@ -4,14 +4,14 @@ Tf = 10;
 Ts = 0.1;
 name = "circle";
 Task_Space = task_traj(X0, Xf, Tf, Ts,name);
-% Q = zeros((Tf/Ts)+1,4);
-% for i = 1 : (Tf/Ts)+1
-%     A = Task_Space(i,:);
-%     if i == 1
-%         q0  = [19*pi/36 25*pi/72 0 pi];
-%     end
-%     if i> 1
-%         q0 = Q(i-1,:);
-%     end
-%     Q(i,:) = (inverse_kinematics_func(q0, A))';
-% end
+Q = zeros((Tf/Ts)+1,4);
+for i = 1 : (Tf/Ts)+1
+    A = Task_Space(i,:);
+    if i == 1
+        q0  = [19*pi/36 25*pi/72 0 pi];
+    end
+    if i> 1
+        q0 = Q(i-1,:);
+    end
+    Q(i,:) = (inverse_kinematics_func(q0, A))';
+end
